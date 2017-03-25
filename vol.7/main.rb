66652a8e2f70bd4c585dfa3d6394c202b09cc63d
PharaohKJ <kato+github@phalanxware.com>
2017-03-25 10:59:02 +0900

    Update main.rb

--- vol.7/main.rb
+++ vol.7/main.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-namespace = 'kato'
+namespace = ENV['MYNAME'] || 'kato'
+device_id = 'BME280'
 
 require 'rubygems'
 gem 'serialport','>=1.0.4'
@@ -15,7 +16,7 @@ loop do
   next if v.length < 10
   time = Time.now.xmlschema # iso8601 型式で現在を取得
   tm = v[10].split('=')[1].to_i/100.0 # 温度は100倍されてくるので割る。
-  v = "aws cloudwatch put-metric-data --namespace #{namespace} --dimensions DeviceId=Terumo --metric-name Temp --timestamp #{time} --value #{tm}"
+  v = "aws cloudwatch put-metric-data --namespace #{namespace} --dimensions DeviceId=#{device_id} --metric-name Temp --timestamp #{time} --value #{tm}"
   puts v # コマンド表示
   puts system v # 実行 & 結果表示(成功すると true 失敗すると false が表示される)
 end
