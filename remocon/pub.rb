ed3e89dda63def40debbf943c8dfd48cbfcfd9bc
PharaohKJ <kato+github@phalanxware.com>
2017-09-01 19:40:36 +0900

    デモに向けてシンプルにした

--- remocon/pub.rb
+++ remocon/pub.rb
@@ -19,7 +19,7 @@ require 'time'
 
 def send_to_aws(metric_name, v, position)
   tm = v[position].split('=')[1].to_i/100.0
-  @client.publish("topic/test", "#{metric_name} #{tm}")
+  @client.publish("topic/test", "#{tm}")
 end
   
 sp = SerialPort.new('/dev/ttyUSB0', 115200, 8, 1, 0) # 115200, 8bit, stopbit 1, parity none
@@ -28,6 +28,6 @@ loop do
   values = line.to_s.strip.split(':')
   next if values.length < 10
   send_to_aws('Temp', values, 10)
-  send_to_aws('Hum', values, 11)
-  send_to_aws('At', values, 12)
+  # send_to_aws('Hum', values, 11)
+  # send_to_aws('At', values, 12)
 end
