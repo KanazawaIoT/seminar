4131a2bcb7fa1f9ca5e4947b4a1ea9edd762f5b1
Daisuke AIBA <aiba@dreamgarage.jp>
2018-09-26 16:44:18 +0900

    計測値の換算処理の修正とループ制御の仕様を変更
    シリアルポートから取得したデータのパース処理を整理し、全て1/100にしていた計測値の換算処理のルールを個別に指定できるように変更
    cronでのバックグランドでの実行を想定して1回の計測値を取得したら処理を終了するように変更

--- vol.7/main.rb
+++ vol.7/main.rb
@@ -3,25 +3,57 @@
 NAMESPACE = ENV['MYNAME'] || 'kato'
 DEVICE_ID = 'BME280'
 
+require 'bigdecimal'
 require 'rubygems'
 gem 'serialport','>=1.0.4'
 require 'serialport'
 require 'time'
 
-def send_to_aws(metric_name, v, position)
-  time = Time.now.xmlschema # iso8601 型式で現在を取得
-  tm = v[position].split('=')[1].to_i/100.0
-  cmd = "aws cloudwatch put-metric-data --namespace #{NAMESPACE} --dimensions DeviceId=#{DEVICE_ID} --metric-name #{metric_name} --timestamp #{time} --value #{tm}"
+def parse(line)
+  values = {}
+  line.to_s.strip.split(':').each{|item|
+    set = item.split("=", 2)
+    key = set[0]
+    value = set[1]
+    next if key.nil?
+    values[key] = value
+  }
+  return values
+end
+
+def put_metric_data(metric_name, timestamp, value)
+  cmd = "aws cloudwatch put-metric-data --namespace \"#{NAMESPACE}\" --dimensions DeviceId=#{DEVICE_ID} --metric-name \"#{metric_name}\" --timestamp #{timestamp} --value #{value}"
   puts cmd # コマンド表示
   puts system cmd # 実行 & 結果表示(成功すると true 失敗すると false が表示される)
 end
-  
-sp = SerialPort.new('/dev/ttyUSB0', 115200, 8, 1, 0) # 115200, 8bit, stopbit 1, parity none
+
+serialport = SerialPort.new('/dev/ttyUSB0', 115200, 8, 1, 0) # 115200, 8bit, stopbit 1, parity none
 loop do
-  line = sp.gets # read
-  values = line.to_s.strip.split(':')
-  next if values.length < 10
-  send_to_aws('Temp', values, 10)
-  send_to_aws('Hum', values, 11)
-  send_to_aws('At', values, 12)
+  timestamp = Time.now.xmlschema # iso8601 型式で現在時刻を取得
+
+  line = serialport.gets # read
+  values = parse(line)
+
+  next if values['tm'].nil? && values['hu'].nil? && values['at'].nil?
+
+  # 温度
+  unless values['tm'].nil?
+    tm = BigDecimal(values['tm']) * 0.01
+    put_metric_data('Temperature', timestamp, tm.floor(2).to_f)
+  end
+
+  # 湿度
+  unless values['hu'].nil?
+    hu = BigDecimal(values['hu']) * 0.01
+    put_metric_data('Humidity', timestamp, hu.floor(2).to_f)
+  end
+
+  # 気圧
+  unless values['at'].nil?
+    at = values['at']
+    put_metric_data('Atmospheric Pressure', timestamp, at)
+  end
+
+  break
+
 end
